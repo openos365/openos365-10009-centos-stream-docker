5e03b9d5c4a94aa1a79e92a6d909d69d7c425057
gnuhub <gnuhub@gmail.com>
2023-09-18 23:37:07 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -6,6 +6,13 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 echo $PROJECT_NAME
 cd $CMD_PATH
 
+git config --global user.email "gnuhub@gmail.com"
+git config --global user.name "gnuhub"
+
+git remote -v
+git remote set-url origin git@github.com:${GITHUB_REPOSITORY}.git
+git remote -v
+
 ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "frs.sourceforge.net"
 ssh-keyscan "frs.sourceforge.net" >> /home/runner/.ssh/known_hosts
 ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "github.com"
@@ -16,6 +23,7 @@ cat /home/runner/.ssh/known_hosts
 env
 
 cd $CMD_PATH
+
 docker build . -f Dockerfile \
 -t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER \
 -t ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest \
@@ -35,12 +43,18 @@ docker push registry.cn-hangzhou.aliyuncs.com/${GITHUB_REPOSITORY}-$GITHUB_REF_N
 # docker push hkccr.ccs.tencentyun.com/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:latest 
 docker push gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER
 docker push gnuhub/$PROJECT_NAME-$GITHUB_REF_NAME:latest
+cd $CMD_PATH
 
-git remote -v
-# cd ~/
-# git clone git@github.com:archlinux365/9996-ubuntu-docker.git
+cid=$(docker run -it --detach ghcr.io/${GITHUB_REPOSITORY}-$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER)
+docker cp ${cid}:/root/versions/ ./versions/
 
-# cd 9996-ubuntu-docker
+cd $CMD_PATH
+git add .
+git commit -a -m "CI-BOT:$(date +%Y.%m.%d-%H%M%S)-$GITHUB_REF_NAME-$GITHUB_RUN_NUMBER"
+git push origin HEAD
+
+
+
 
 # rm -rf versions 
 
